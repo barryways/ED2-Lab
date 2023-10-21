e57ac0c92a4bca5bfbc34da84adab936b388a2d9
Carlos Barrientos <barryphotos7@gmail.com>
2023-10-20 23:26:03 -0600

    Adding some changes

--- Backend/src/util/RSA/RSA2.js
+++ Backend/src/util/RSA/RSA2.js
@@ -1,81 +1,62 @@
-// Función para calcular el máximo común divisor (MCD)
-function gcd(a, b) {
-    if (b === 0n) return a;
-    return gcd(b, a % b);
+
+import BigNumber from 'bignumber.js'
+
+export default class GeneratorKey {
+  constructor() {
+    this.KEY_SIZE = 256; // Tamaño de clave más grande (en bits)
   }
-  
-  // Función para calcular un número primo aleatorio
-  function generateRandomPrime(bits) {
-    let min = 2n ** (bits - 1n);
-    let max = 2n ** bits - 1n;
-  
-    while (true) {
-      const num = min + BigInt(Math.floor(Math.random() * (Number(max - min) + 1)));
-      if (isPrime(num)) {
-        return num;
-      }
-    }
+
+  generatePrivateKey(k, z, n) {
+    const j = this.extEuclid(k, z)[1];
+    return new PrivateKey(n, j, z);
   }
-  
-  // Función para verificar si un número es primo
-  function isPrime(num) {
-    if (num <= 1n) return false;
-    if (num <= 3n) return true;
-    if (num % 2n === 0n || num % 3n === 0n) return false;
-  
-    let i = 5n;
-    while (i * i <= num) {
-      if (num % i === 0n || num % (i + 2n) === 0n) return false;
-      i += 6n;
-    }
-  
-    return true;
+
+  extEuclid(a, b) {
+    if (b.isZero())
+      return [a, new BigNumber(1), new BigNumber(0)];
+
+    const vals = this.extEuclid(b, a.mod(b));
+    const d = vals[0];
+    const p = vals[2];
+    const q = vals[1].minus(a.dividedToIntegerBy(b).times(vals[2]));
+    return [d, p, q];
   }
-  
-  class SimpleRSA {
-    constructor(bits) {
-      const p = generateRandomPrime(bits);
-      const q = generateRandomPrime(bits);
-      this.p = p;
-      this.q = q;
-      this.n = p * q;
-      this.phi = (p - 1n) * (q - 1n);
-      this.e = 65537n; // Usar un valor común para e (65537) para eficiencia
-      this.d = this.computeDecryptionExponent(this.e, this.phi);
-      this.publicKey = { e: this.e, n: this.n };
-      this.secretKey = { d: this.d, n: this.n };
-    }
-  
-    computeDecryptionExponent(e, phi) {
-      let d = extendedGcd(e, phi).s;
-  
-      while (d < 1n) {
-        d += phi;
-      }
-  
-      return d;
-    }
-  
-    encrypt(m) {
-      if (m < 0n || m >= this.n) {
-        throw new Error(`Condition 0 <= m < n not met. m = ${m}`);
-      }
-  
-      if (gcd(m, this.n) !== 1n) {
-        throw new Error("Condition gcd(m, n) = 1 not met.");
-      }
-  
-      const c = m ** this.e % this.n;
-  
-      return c;
-    }
-  
-    decrypt(c) {
-      const m = c ** this.d % this.n;
-  
-      return m;
+
+  generatePublicKey() {
+    const p = this.generarPrimo();
+    const q = this.generarPrimo();
+    const n = p.times(q);
+    const z = p.minus(1).times(q.minus(1));
+    const k = this.obtenerCoprimo(z);
+    return new PublicKey(n, k, z);
+  }
+
+  getKeys() {
+    const publicKey = this.generatePublicKey();
+    const privateKey = this.generatePrivateKey(publicKey.getK(), publicKey.getZ(), publicKey.getN());
+    return new Keys(privateKey, publicKey);
+  }
+
+  generarPrimo() {
+    return new BigNumber(this.KEY_SIZE).prime(this.KEY_SIZE);
+  }
+
+  calcularMCD(a, b) {
+    if (b.isZero()) {
+      return a;
+    } else {
+      return this.calcularMCD(b, a.mod(b));
     }
   }
-  
-  export default SimpleRSA;
-  
+
+  obtenerCoprimo(z) {
+    const e = new BigNumber(this.KEY_SIZE);
+    const rand = BigNumber.random(this.KEY_SIZE);
+    do {
+      while (e.isLessThanOrEqualTo(z)) {
+        e = new BigNumber(this.KEY_SIZE);
+      }
+    } while (!this.calcularMCD(e, z).isEqualTo(1));
+    return e;
+  }
+}
